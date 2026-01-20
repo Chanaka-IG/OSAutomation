aa66c03dadfff99110e9da85af579d07cae28900
Chanaka Prasad <igchanaka.prasad@gmail.com>
2026-01-20 21:42:36 +0530

    add seperate file for PIM api data load

--- tests/employee/pimAccessTest.spec.ts
+++ tests/employee/pimAccessTest.spec.ts
@@ -1,7 +1,7 @@
 import { test, request } from '@playwright/test';
 import { PimAccessPage } from '../../pages/PIM/PimAccessPage';
-import { EmployeeMaster } from '../../api/masterdata/EmployeeMaster';
-import { UsesMaster } from '../../api/masterdata/UsesMaster';
+import { AddEmployee } from '../../api/Employee/AddEmployee';
+import { AddUsers } from '../../api/Admin/AddUsers'
 
 
 import { PIM_DATA } from '../../data/PIM'
@@ -11,14 +11,14 @@ test.describe("PIM Validation", async () => {
 
   let employeeAccessPage: PimAccessPage;
 
-  // test.beforeAll(async ({ browser }) => {
-  //   const apiContext = await request.newContext();
-  //   const employee = new EmployeeMaster(apiContext);
-  //   const users = new UsesMaster(apiContext);
-  //   await employee.loginAsAdmin();
-  //   await employee.addEmployees(PIM_DATA.API_date.employee);
-  //   await users.addUsers(PIM_DATA.API_date.user)
-  // })
+  test.beforeAll(async ({ browser }) => {
+    const apiContext = await request.newContext();
+    const employee = new AddEmployee(apiContext);
+    const users = new AddUsers(apiContext);
+    await employee.loginAsAdmin();
+    await employee.addEmployees(PIM_DATA.API_date.employee);
+    await users.addUsers(PIM_DATA.API_date.user)
+  })
 
 
   test.beforeEach(async ({ page }) => {
@@ -27,14 +27,14 @@ test.describe("PIM Validation", async () => {
   })
 
 
-  test('1. Validate Admin has the access to view the PIM', async ({ page }) => {
+  test('1. Validate Admin has the access to view the PIM', async () => {
     await employeeAccessPage.loginasCustomAdmin(PIM_DATA.API_date.user[0].username,PIM_DATA.API_date.user[0].password);
     await employeeAccessPage.validateUIasAdmin()
 
   });
 
 
-  test('2. Validate Admin has the access to view the PIM', async ({ page }) => {
+  test('2. Validate ESS has no access to view the PIM', async () => {
     await employeeAccessPage.loginasCustomESS(PIM_DATA.API_date.user[1].username,PIM_DATA.API_date.user[1].password);
     await employeeAccessPage.validateUIasESS()
 
