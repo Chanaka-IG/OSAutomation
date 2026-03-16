8953f726d37d2aef82b8b88e091ded97188169f9
Chanaka Prasad <igchanaka.prasad@gmail.com>
2026-03-16 21:16:41 +0530

    added new improvments to validate report data

--- tests/employee/report.spec.ts
+++ tests/employee/report.spec.ts
@@ -3,6 +3,7 @@ import { ReportPage } from '../../pages/PIM/ReportPage';
 import { LogAsAdmin } from '../../api/logAsAdmin'
 import { AddEmployee } from '../../api/Employee/AddEmployee'
 import { ReportData } from '../../data/PIM/report'
+import { UpdateEmployee } from '../../api/Employee/UpdateEMployee'
 
 
 test.describe("Test cases for Report page in PIM module", () => {
@@ -10,12 +11,26 @@ test.describe("Test cases for Report page in PIM module", () => {
     let logAdmin: LogAsAdmin;
     let addEmployee: AddEmployee;
     let reportPage: ReportPage;
+    let updateEmployee: UpdateEmployee;
 
     test.beforeAll(async ({ request }) => {
         logAdmin = new LogAsAdmin(request)
         addEmployee = new AddEmployee(request)
-        // await logAdmin.loginAsAdmin();
-        // await addEmployee.addEmployees(ReportData.AddEmployee);
+        updateEmployee = new UpdateEmployee(request)
+        await logAdmin.loginAsAdmin();
+        await addEmployee.addEmployees(ReportData.AddEmployee);
+        const employeeSet = await addEmployee.getEmployees();
+        const empSet = employeeSet.data;
+        const updateEmpData = ReportData.UpdateEmployee;
+
+        for (const updateEmp of updateEmpData) {
+            for (const empSystem of empSet) {
+                if (updateEmp.employeeId === empSystem.employeeId) {
+                    await updateEmployee.updateEmployeeJobDetails(empSystem.empNumber, updateEmp)
+                }
+            }
+        }
+
     })
 
     test.beforeEach(async ({ page, logger }) => {
@@ -23,7 +38,7 @@ test.describe("Test cases for Report page in PIM module", () => {
         await page.goto("/")
         await reportPage.loginasAdmin();
         await reportPage.navigateToPim();
-        
+
     })
 
     test("1. Validate Report page with a screenshot", async ({ page }) => {
@@ -36,12 +51,27 @@ test.describe("Test cases for Report page in PIM module", () => {
         await reportPage.validateReportPageUIElements();
     })
 
-    test.only("3. Add report test", async ({ page }) => {
+    test("3. Add report ", async ({ page }) => {
         await reportPage.navigateToReportPage();
         await reportPage.waitUntilTableLoaderDissapear();
         await reportPage.clickOnAddBtn();
         await reportPage.waitUntilFormLoaderDissapear();
         await reportPage.fillReportForm(ReportData.AddReport[0]);
+        await reportPage.clickOnSaveBtn();
+        await reportPage.verifySuccessToastForSave();
+        await reportPage.waitUntilFormLoaderDissapear();
+    })
+
+    test.only("4. Add report and validate data", async ({ page }) => {
+        await reportPage.navigateToReportPage();
+        await reportPage.waitUntilTableLoaderDissapear();
+        await reportPage.clickOnAddBtn();
+        await reportPage.waitUntilFormLoaderDissapear();
+        await reportPage.fillReportForm(ReportData.AddReport[0]);
+        await reportPage.clickOnSaveBtn();
+        await reportPage.verifySuccessToastForSave();
+        await reportPage.waitUntilFormLoaderDissapear();
+        await reportPage.validateInReport(ReportData.validateReport);
     })
 
 
